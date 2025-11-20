07b47416ddfde26cc693162f10b6edc8fb1278ba
inpicksys <inpicksys@gmail.com>
2025-11-20 16:13:56 +0000

    Deploying to gh-pages from @ inpicksys/inpicksys.github.io@83fdc3975ce48155622963a3ef2165592c1762aa 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -528,14 +528,21 @@ ninja.data = [{
         title: 'CV',
         section: 'Socials',
         handler: () => {
-          window.open("/assets/pdf/example_pdf.pdf", "_blank");
+          window.open("/assets/pdf/Mikhail_Strutinsky_-_Senior_Fullstack_Developer_latest.pdf", "_blank");
         },
       },{
         id: 'social-email',
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%6D%69%6B%68%61%69%6C.%73%74%72%75%74%69%6E%73%6B%79@%67%6D%61%69%6C.%63%6F%6D", "_blank");
+        },
+      },{
+        id: 'social-github',
+        title: 'GitHub',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://github.com/inpicksys", "_blank");
         },
       },{
         id: 'social-inspire',
@@ -543,6 +550,20 @@ ninja.data = [{
         section: 'Socials',
         handler: () => {
           window.open("https://inspirehep.net/authors/1010907", "_blank");
+        },
+      },{
+        id: 'social-linkedin',
+        title: 'LinkedIn',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.linkedin.com/in/mikhail-s-996581a5", "_blank");
+        },
+      },{
+        id: 'social-medium',
+        title: 'Medium',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://medium.com/@strmisha.s", "_blank");
         },
       },{
         id: 'social-rss',
@@ -556,7 +577,21 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("https://scholar.google.com/citations?user=cGALo4gAAAAJ&hl", "_blank");
+        },
+      },{
+        id: 'social-coursera_id',
+        title: 'Coursera_id',
+        section: 'Socials',
+        handler: () => {
+          window.open("", "_blank");
+        },
+      },{
+        id: 'social-udemy_id',
+        title: 'Udemy_id',
+        section: 'Socials',
+        handler: () => {
+          window.open("", "_blank");
         },
       },{
       id: 'light-theme',
